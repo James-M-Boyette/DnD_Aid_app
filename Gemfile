433c299d778923f8f72de0d7b944f5efe0bbb65e
James Boyette <james.boyette@gmail.com>
2021-04-15 20:11:26 -0400

    Add gem of and test RESpec code to app

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.2'
 end
 
 group :development do
@@ -44,3 +45,4 @@ gem 'http'
 gem 'rack-cors'
 gem 'jb'
 gem 'seed_dump'
+
---
+++ spec/requests/alignments_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "Rlignments", type: :request do
+  describe "GET /alignments" do
+    it "should return all the alignments in the db" do
+      # user = User.create!(
+      #   email: "brian@email.com",
+      #   password: "password",
+      # )
+
+      Alignment.create!(
+        id: "the alignment id",
+        name: "the alignment name",
+        abbreviation: "the alignment abbreviation",
+        desc: "the alignment desc",
+        imgurl: "the alignment imgurl",
+      )
+      # Here, we've hard-coded stuff for each ... name could be "Uber Good" instead of "the alignment name"
+
+      get "/api/alignments"
+      alignments = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(alignments.length).to eq(1)
+    end
+    describe "POST /alignments" do
+      it "should create a new recipe in the db" do
+        post "/api/alignments", params {
+          id: "the id 2",
+          name: "the name 2",
+          abbreviation: "the abbreviation 2",
+          desc: "the desc 2",
+          imgurl: "the imgurl 2",
+        }
+      end
+  end
+end
