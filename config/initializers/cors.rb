5ab940a884862b54a0179d91226b8a0a60eeba45
James Boyette <james.boyette@gmail.com>
2021-06-01 01:06:55 -0400

    CORS fix 1b: update origin addresses to include non-final forward slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["localhost:8080", "https://dnd-aid.netlify.app/"]
+    origins ["localhost:8080", "https://dnd-aid.netlify.api", "https://dnd-aid.netlify.api/"]
 
     resource "*",
       headers: :any,
